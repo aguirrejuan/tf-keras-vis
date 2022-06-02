Fixed mistake in ScoreCam with weight softmax

tf_keras_vis/scorecam.py
@@ -162,9 +162,10 @@ class Scorecam(ModelVisualization):
                  for prediction in listify(preds))
         
         # Calculating weights
-        weights = ([score(p) for p in prediction] 
-                   for score, prediction in zip(scores, preds)) 
-        weights = (K.softmax(prediction,axis=0) for prediction in preds)
+        weights = [[score(p) for p in prediction] 
+                   for score, prediction in zip(scores, preds)]
+        weights = np.concatenate(weights)
+        weights = (K.softmax(weights,axis=0),)
         weights = ([self._validate_weight(s, nsamples) for s in w] for w in weights)
         weights = (np.array(w, dtype=np.float32) for w in weights)
         weights = (np.reshape(w, (channels, nsamples, -1)) for w in weights)